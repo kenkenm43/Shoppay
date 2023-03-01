f9000ae46344bf4d4927169bbc11f6a6339f1ea8
kenkenm43 <kenkenm43@gmail.com>
2023-03-01 20:23:08 +0700

    FIX url protocol

--- next.config.js
+++ next.config.js
@@ -1,6 +1,39 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, '/src/styles')],
+    prependData: ``,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.britannica.com',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**'
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
